131ec9216e57b140867bb6b37b2fbad8591f4989
Supriya <supriyapandian004@gmail.com>
2024-07-11 04:50:50 -0700

    new changes

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js or pages/index.tsx
+import React from 'react'
+function Home(){
+  return (
+   <h1 style={{textAlign:'center',margin:'10'}}>hello mama</h1>
+  );
+};
+
+export default HomePage;
